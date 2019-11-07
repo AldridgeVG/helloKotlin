4e0a90ddebcf57f92621b0fd9b6d8baf4a09feb8
aldridgevg <caxuser@live.com>
2019-11-07 20:42:14 +0800

    adding try catch

--- src/helloNullPointer.kt
+++ src/helloNullPointer.kt
@@ -1,21 +1,59 @@
-fun main(args:Array<String>){
+import java.lang.Exception
+import java.lang.IllegalStateException
 
-    println("plz input your name:")
-    var name = readLine()
-//  name can be assigned as null
-//    name = null
-    println("your name is $name")
+class namenullIsNullException() : IllegalStateException("namenull is null")
+
+fun nullSecurityCheck(namenull: Int?) {
+    namenull ?: throw namenullIsNullException()
+}
+
+fun nullCapicityCheck(namenull: Int) {
+    require(namenull >= 3, { "namenull is smaller than 3!" })
+}
+
+fun main(args: Array<String>) {
+
+    val name = "aldridgevg"
+//    val namenull:String? = null
+//    throw a exception
+//    namenull?:throw IllegalStateException("namenull is null")
+//    throw customized exception
+//    namenull?:namenullIsNull()
+
+
+    //throw exception in 1/3 chance use try-catch
+    //catch:print exception info and continue running
+    var namenull: Int? = null
+    val check = (1..3).shuffled().last() == 3
+    if (check)
+        namenull = 2
+    try {
+        nullSecurityCheck(namenull)
+        namenull = namenull!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
+    println("namenull = " + namenull)
+
+    val namenull1 = (1..3).shuffled().first()
+    try {
+        nullCapicityCheck(namenull1)
+    } catch (e: Exception) {
+        println(e)
+    }
 
     println("input name1:")
-    var name1 = readLine()    //use safe call operator "?"
+    var name1 = readLine()    //use safe call operator "?"(readLine return a String?)
     println("input name2:")
-    var name2 = readLine()?.let{            //use safe call operator "let"
-        if(it.isNotBlank()) it.capitalize()
-        else                "null"
+    var name2 = readLine()?.let {
+        //use safe call operator "let"
+        if (it.isNotBlank()) it.capitalize()
+        else "null"
     }
     println("input name3:")
     var name3 = readLine()!!.capitalize()   //strong request to call, if null catch KotlinNullPointerException
-    val name4:String = name1!!.capitalize()?:"aldridgeVG"  //use null combine operator - if left of "?:" is null, use right
-    println("name1: $name1\nname2: $name2\nname3: $name3\nname4(capt of name 1): $name4")
+    val name4: String = name1 ?: name  //use null combine operator - if left of "?:" is null, use right
+    println("name1: $name1\nname2: $name2\nname3: $name3\nname4(clone of name1 or name): $name4")
 
+    
 }
